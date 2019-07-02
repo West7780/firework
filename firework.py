7acb67941cc160e1be55281d8cd956e26aedf80e
West <westly7rock@gmail.com>
2019-07-01 22:39:07 -0400

    Add files via upload

--- firework.py
+++ firework.py
@@ -1,7 +1,3 @@
-#set mode
-#mode = 'fire'
-mode = input('rain or fire? ')
-print('Loading',end="")
 import time,random,pygame,math
 
 def genNeon():
@@ -12,31 +8,19 @@ def genNeon():
         nums2 += [i]
         nums.remove(i)
     return nums2
-
-def genVector(length):
-    num1 = random.uniform(0,abs(length))
-    num2 = length-num1
-    num3 = random.randint(0,1)
-    if num3 == 0: num3 = -1
-    num4 = random.randint(0,1)
-    if num4 == 0: num4 = -1
-    return num1*num3,num2*num4
-
+    
 class particle():
-    def __init__(self,x,y,color = (255,255,255),size = 2,lifespan=0):
+    def __init__(self,x,y,color = (255,255,255),size = 2):
         self.pos = [x,y]
         self.vel = [0,0]
         self.size = size
         self.color = color
-        self.timealive = 0
-        self.lifespan = lifespan
-        
+
     def applyForce(self,force):
         self.vel[0] += force[0]
         self.vel[1] += force[1]
     
     def update(self):
-        if self.lifespan>0: self.timealive+= 1
         self.pos[0] += self.vel[0]
         self.pos[1] += self.vel[1]
 
@@ -46,34 +30,31 @@ class particle():
 Gravity = (0,.01)
 
 pygame.init()
+
 width,height = 500,500
 pygame.display.init()
 pygame.display.set_mode((width,height),pygame.RESIZABLE)
 pygame.display.set_icon(pygame.Surface((32,32)))
-pygame.mouse.set_visible(0)
-mouseColor = genNeon()
 
-raindrops = []
-bursting = []
+fireworks = []
 stars = []
 
 while True:
     start = time.time()
-    pygame.display.set_caption(mode+"    Particles:"+str(len(raindrops+bursting+stars)))
+    pygame.display.set_caption("Firework    Particles:"+str(len(fireworks+stars)))
     pygame.display.get_surface().fill((5,5,5),special_flags = pygame.BLEND_RGB_SUB)
 
-    for part in bursting:
+    for part in fireworks:
         
         part.show()
         part.applyForce(Gravity)
         part.update()
         
-        if part.vel[1] >= random.random()*-1 or int(part.pos[0]) not in range(0,width) or int(part.pos[1]) not in range(0,height):
+        if part.vel[1] >= -1 or int(part.pos[0]) not in range(0,width) or int(part.pos[1]) not in range(0,height):
             for x in range(0,25):
-                force = genVector(random.uniform(0.1,0.5))
-                stars+= [particle(int(part.pos[0]),int(part.pos[1]),part.color,0.79,150)]
-                stars[len(stars)-1].applyForce((force[0]+part.vel[0],force[1]+part.vel[1]))
-            bursting.remove(part)
+                stars+= [particle(int(part.pos[0]),int(part.pos[1]),part.color,0.79)]
+                stars[len(stars)-1].applyForce((random.uniform(-0.5,0.5)+part.vel[0],random.uniform(-0.5,0.5)+part.vel[1]))
+            fireworks.remove(part)
 
     for part in stars:
 
@@ -81,38 +62,15 @@ while True:
         part.applyForce(Gravity)
         part.update()
         
-        if part.timealive>=part.lifespan or int(part.pos[0]) not in range(0,width) or int(part.pos[1]) not in range(0,height):
-            stars.remove(part)
-
-    for part in raindrops:
-
-        part.show()
-        part.applyForce(Gravity)
-        part.update()
-        
         if int(part.pos[0]) not in range(0,width) or int(part.pos[1]) not in range(0,height):
-            raindrops.remove(part)
-
-    pygame.draw.circle(pygame.display.get_surface(), mouseColor, pygame.mouse.get_pos(), 5)
-
-    if mode == 'fire':
-        #create burst bursting
-        if random.randint(0,10) == 1:
-            bursting+= [particle(random.randint(0,width),height,genNeon())]
-            bursting[len(bursting)-1].applyForce((random.uniform(-.2,.2),random.uniform(-3,-1.375)))
-    elif mode == 'rain':
-        #create rain
-        if random.randint(0,7) == 1:
-            raindrops+= [particle(random.randint(0,width),0,genNeon())]
-    else:
-        #create burst bursting
-        if random.randint(0,10) == 1:
-            bursting+= [particle(random.randint(0,width),height,genNeon())]
-            bursting[len(bursting)-1].applyForce((random.uniform(-.2,.2),random.uniform(-3,-1.375)))
-       #create rain
-        if random.randint(0,7) == 1:
-            raindrops+= [particle(random.randint(0,width),0,genNeon())]
-            
+            stars.remove(part)
+            pass
+        
+    #create fireworks
+    if random.randint(0,10) == 1:
+        fireworks+= [particle(random.randint(0,width),height,genNeon())]
+        fireworks[len(fireworks)-1].applyForce((random.uniform(-.2,.2),random.uniform(-3,-1.375)))
+        
     pygame.display.flip()
 
     #handle events
@@ -122,16 +80,8 @@ while True:
         if event.type == pygame.VIDEORESIZE:
             width,height = event.w,event.h
             pygame.display.set_mode((width,height),pygame.RESIZABLE)
-        if event.type == pygame.MOUSEBUTTONUP:
-            if 1:
-                for x in range(0,150):
-                    stars+= [particle(event.pos[0],event.pos[1],mouseColor,1)]
-                    stars[len(stars)-1].applyForce(genVector(random.uniform(0,1.25)))
-            else:
-                raindrops+= [particle(event.pos[0],event.pos[1],mouseColor,4)]
-            mouseColor = genNeon()
 
     end = time.time()
     elapsed = end - start
-    if elapsed < 0.02:
-        time.sleep(0.02-elapsed)
+    if elapsed < 0.015:
+        time.sleep(0.015-elapsed)
